93f3b62f74c420b8c895f8e40e24ce6dd944021f
DropboxKenshiro <jatujestem144@gmail.com>
2019-11-21 19:07:28 +0100

    SidePanel upgrade.

--- scripts/SidePanel.gd
+++ scripts/SidePanel.gd
@@ -1,2 +1,11 @@
 extends Panel
 
+onready var hero = $"../../../Hero"
+
+export (PoolColorArray) var class_color_list
+
+func _ready():
+	$HeroClass.text = hero.hero_name
+	$HeroClass.modulate = self.class_color_list[hero.hero_class]
+	
+	$Level.text = "LVL" + String(hero.level).pad_zeros(2)
